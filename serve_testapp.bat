534ab3e85fffeb8b0f058e872da865e9695cddca
Amber Yu <ky108@duke.edu>
2022-02-01 18:38:28 -0500

    0

--- serve_testapp.bat
+++ serve_testapp.bat
@@ -1,4 +1,4 @@
-cd C:\Users\vcm\Desktop\testapp
-git pull https://%TESTAPP_GIT_PAT%@github.com/JakeVestal/testapp.git
+cd C:\Users\ky108\PycharmProjects\testapp
+git pull https://%TESTAPP_GIT_PAT%git@github.com:ykamber99/testapp.git
 venv\Scripts\python.exe -m pip install -r requirements.txt
 venv\Scripts\python.exe server.py
