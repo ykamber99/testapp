e1f259c348b713704459b35d248e03dd7befa840
Amber Yu <ky108@duke.edu>
2022-02-03 15:12:18 -0500

    0

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Dash(__name__)
 server = app.server
 app.layout = html.Div([
     github_info_header(),
-    html.Img(src="assets/tvol_banner.png")
+    html.Img(src="assets/dog.jpg")
 ])
 
 if __name__ == '__main__':
